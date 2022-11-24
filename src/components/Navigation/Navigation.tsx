1576aa91da4934b5f28f6279897f340283ddb788
Nicholas Huynh <nickhuynhq@gmail.com>
2022-11-24 13:38:27 -0500

    Fix hamburger nav pushing content down in Safari

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,90 +1,91 @@
 import React, { useState } from "react";
-import { CgMenuGridR } from "react-icons/cg"
-import {RiFullscreenExitLine} from "react-icons/ri"
+import { CgMenuGridR } from "react-icons/cg";
+import { RiFullscreenExitLine } from "react-icons/ri";
 import "./Navigation.scss";
 
 const Navigation = () => {
   const [menuView, setMenuView] = useState(false);
 
   return (
-    <nav className="navigation">
-      <div className="navigation__hamburger-container">
-        <button
-          className="navigation__hamburger"
-          onClick={() => setMenuView(!menuView)}
-        >
+    <div className="navigation-container">
+      <nav className="navigation">
+        <div className="navigation__hamburger-container">
+          <button
+            className="navigation__hamburger"
+            onClick={() => setMenuView(!menuView)}
+          >
+            {menuView ? <RiFullscreenExitLine /> : <CgMenuGridR />}
+          </button>
 
-         { menuView ? <RiFullscreenExitLine/> : <CgMenuGridR />}
-        </button>
+          <a className="navigation__home" href="/">
+            nicholashuynh.
+          </a>
+        </div>
+        {menuView && (
+          <ul className="navigation__hamburger-list">
+            <li>
+              <a
+                className="navigation__hamburger-list-item"
+                href="#about"
+                onClick={() => setMenuView(!menuView)}
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className="navigation__hamburger-list-item"
+                href="#projects"
+                onClick={() => setMenuView(!menuView)}
+              >
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                className="navigation__hamburger-list-item"
+                href="#experience"
+                onClick={() => setMenuView(!menuView)}
+              >
+                Experience
+              </a>
+            </li>
+            <li>
+              <a
+                className="navigation__hamburger-list-item"
+                href="#contact"
+                onClick={() => setMenuView(!menuView)}
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        )}
 
-        <a className="navigation__home" href="/">
-          nicholashuynh.
-        </a>
-      </div>
-      {menuView && (
-        <ul className="navigation__hamburger-list">
+        <ul className="navigation__list">
           <li>
-            <a
-              className="navigation__hamburger-list-item"
-              href="#about"
-              onClick={() => setMenuView(!menuView)}
-            >
+            <a className="navigation__list-item" href="#about">
               About
             </a>
           </li>
           <li>
-            <a
-              className="navigation__hamburger-list-item"
-              href="#projects"
-              onClick={() => setMenuView(!menuView)}
-            >
+            <a className="navigation__list-item" href="#projects">
               Projects
             </a>
           </li>
           <li>
-            <a
-              className="navigation__hamburger-list-item"
-              href="#experience"
-              onClick={() => setMenuView(!menuView)}
-            >
+            <a className="navigation__list-item" href="#experience">
               Experience
             </a>
           </li>
           <li>
-            <a
-              className="navigation__hamburger-list-item"
-              href="#contact"
-              onClick={() => setMenuView(!menuView)}
-            >
+            <a className="navigation__list-item" href="#contact">
               Contact
             </a>
           </li>
         </ul>
-      )}
-
-      <ul className="navigation__list">
-        <li>
-          <a className="navigation__list-item" href="#about">
-            About
-          </a>
-        </li>
-        <li>
-          <a className="navigation__list-item" href="#projects">
-            Projects
-          </a>
-        </li>
-        <li>
-          <a className="navigation__list-item" href="#experience">
-            Experience
-          </a>
-        </li>
-        <li>
-          <a className="navigation__list-item" href="#contact">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
