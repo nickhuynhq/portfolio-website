e1d691f347bdaa2f16541545040ba4afefe0b78d
Nicholas Huynh <nickhuynhq@gmail.com>
2023-03-09 16:03:06 -0500

    Fit footer issue and add bounce effect to all arrows

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -1,58 +1,63 @@
 import "./Contact.scss";
-import Footer from "../../components/Footer/Footer";
+import { BsChevronDoubleDown } from "react-icons/bs";
 
 const Contact = () => {
   return (
     <section id="contact" className="contact">
-      <h1 className="contact__heading">{`// Contact Me`}</h1>
-      <p className="contact__text">
-        I am interested in full-time and freelance opportunities- especially on
-        ambitious or large projects. If you have any other requests or
-        questions, please don't hesitate to reach me using the form below.
-      </p>
+      <div className="contact__main">
+        <h1 className="contact__heading">{`// Contact Me`}</h1>
+        <p className="contact__text">
+          I am interested in full-time and freelance opportunities- especially
+          on ambitious or large projects. If you have any other requests or
+          questions, please don't hesitate to reach me using the form below.
+        </p>
 
-      <div className="contact__container">
-        <form
-          className="contact__form"
-          action={`https://formsubmit.co/${process.env.REACT_APP_EMAIL}`}
-          method="POST"
-        >
-          <div className="contact__form-top">
+        <div className="contact__container">
+          <form
+            className="contact__form"
+            action={`https://formsubmit.co/${process.env.REACT_APP_EMAIL}`}
+            method="POST"
+          >
+            <div className="contact__form-top">
+              <input
+                className="contact__form-input"
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                required
+              />
+              <input
+                className="contact__form-input"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+
             <input
               className="contact__form-input"
               type="text"
-              name="name"
-              placeholder="Full Name"
+              name="subject"
+              placeholder="Subject"
               required
             />
-            <input
-              className="contact__form-input"
-              type="email"
-              name="email"
-              placeholder="Email"
+            <textarea
+              className="contact__form-textarea"
+              name="message"
+              placeholder="Message"
               required
             />
-          </div>
-
-          <input
-            className="contact__form-input"
-            type="text"
-            name="subject"
-            placeholder="Subject"
-            required
-          />
-          <textarea
-            className="contact__form-textarea"
-            name="message"
-            placeholder="Message"
-            required
-          />
-          <button className="contact__form-button" type="submit">
-            SEND
-          </button>
-        </form>
+            <button className="contact__form-button" type="submit">
+              SEND
+            </button>
+          </form>
+        </div>
       </div>
-      <Footer />
+      <a className="more-arrow bounce" href="#footer">
+        <BsChevronDoubleDown />
+      </a>
+      
     </section>
   );
 };
